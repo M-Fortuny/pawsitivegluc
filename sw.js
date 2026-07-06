e32b8fa2147f3b2793e4be213005dedc4f226555
Manuel Fortuny Ruiz <manuelfortunyruiz@gmail.com>
2026-07-07 00:39:33 +0200

    Add files via upload
    Versión 1

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'diabetes-perros-v1';
+const ASSETS = [
+  './', 
+  './index.html', 
+  './style.css', 
+  './script.js', 
+  './manifest.json'
+  // Si tienes imágenes o iconos, añádelos aquí también
+];
+
+// Instalar el Service Worker y guardar en caché
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Interceptar peticiones y servir desde caché
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
